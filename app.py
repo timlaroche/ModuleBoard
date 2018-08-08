b1856aac75f79becbcf318adf531f37a7eac0a02
Tim La Roche <timlaroche@users.noreply.github.com>
2018-08-08 19:00:55 +0100

    Added sign up and login, passwords hashed and salted

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import *
-import pymongo
+from pymongo import *
+import bcrypt
 app = Flask(__name__)
+client = MongoClient('localhost', 27017)
 
 @app.route("/")
 def hello():
@@ -15,15 +17,37 @@ def admin(name):
 	return render_template('admin.html', name=name)
 
 # User Routes
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
-	return render_template('login.html')
+	if request.method == 'POST':
+		loginform = request.form
+		db = client.test_db
+		collection = db.test_users
+		user = collection.find_one({"username": loginform['username']})
+		hashed = bcrypt.hashpw(loginform['password'].encode('utf-8'), user['salt'])
+		if bcrypt.checkpw(loginform['password'].encode('utf-8'), hashed):
+			return render_template('loginsuccess.html', email=user['email'])
+		else:
+			return "No user with that username/password found."
+	else:
+		return render_template('login.html')
 
 @app.route("/signup", methods=['GET', 'POST'])
 def signup():
 	if request.method == 'POST':
-		#add to db etc
 		myform = request.form
+		#add to db etc
+		db = client.test_db
+		collection = db.test_users
+		userSalt = bcrypt.gensalt()
+		password = bcrypt.hashpw(myform['password'].encode('utf-8'), userSalt)
+		userinfo = {
+			"username": myform['username'],
+			"password": password,
+			"email": myform['email'],
+			"salt": userSalt
+		}
+		print(collection.insert_one(userinfo).inserted_id)
 		return render_template('hello.html', name=myform['username'])
 	else:
 		return render_template('signup.html')
