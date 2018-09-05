05ea5f61ff477f42eb53b8ca656da4fa8bdff02f
Tim La Roche <timlaroche@users.noreply.github.com>
2018-09-05 22:05:31 +0100

    Added basic comments to posts and some general fixes and additions including logout button

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import *
 from pymongo import *
 from bson.json_util import *
 import bcrypt, json, bson
+import datetime
 app = Flask(__name__)
 #need to regenerate this secret key
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
@@ -63,7 +64,7 @@ def modules():
 @app.route("/module/<code>")
 def module(code):
 	#From the database, get the module name and all other details and pass it into the template
-	#render.
+	#render
 	db = client.test_db
 	collection = db.test_modules
 	# Module code, module name
@@ -80,8 +81,28 @@ def modulepost(code, post):
 	db = client.test_db
 	collection = db.test_posts
 	mypost = collection.find_one({"_id" : ObjectId(post)})
-	print(mypost)
-	return render_template("post.html", postinfo=mypost)
+	mycomments = db.test_comments.find({"thread" : str(post)})
+	return render_template("post.html", postinfo=mypost, comments=mycomments)
+
+@app.route("/postcomment", methods=['POST'])
+def postcomment():
+	if session.get('userID'):	
+		db = client.test_db
+		collection = db.test_comments
+		commentform = request.form
+		commentdocument = {
+			"user_id" : session['userID'],
+			"username" : session['username'],
+			"comment" : commentform['comment'],
+			"thread" : commentform['thread'],
+			"date" : datetime.datetime.now(),
+			"time" : datetime.datetime.now(),
+			"karma" : 1 
+		}
+		collection.insert_one(commentdocument)
+		return "Comment posted"
+	else:
+		return "Please login to post comments"
 
 # User Routes
 @app.route("/login", methods=['GET', 'POST'])
@@ -94,11 +115,17 @@ def login():
 		hashed = bcrypt.hashpw(loginform['password'].encode('utf-8'), user['salt'])
 		if bcrypt.checkpw(loginform['password'].encode('utf-8'), hashed):
 			session['userID'] = dumps(user['_id'])
-			return render_template('loginsuccess.html', email=user['email'])
+			session['username'] = user['username']
+			return redirect("/", code=302 )
 		else:
 			return "No user with that username/password found."
 	else:
 		return render_template('login.html')
+
+@app.route("/logout", methods=['POST'])
+def logout():
+	session.clear()
+	return redirect('/', code=302)
 
 @app.route("/signup", methods=['GET', 'POST'])
 def signup():
@@ -115,6 +142,7 @@ def signup():
 			"email": myform['email'],
 			"salt": userSalt
 		}
+		print(userSalt)
 		print(collection.insert_one(userinfo).inserted_id)
 		return render_template('hello.html', name=myform['username'])
 	else:
--- devrun.sh
+++ devrun.sh
@@ -1,4 +1,4 @@
-mongod --fork --dbpath data/db
+mongod --fork --dbpath data/db --logpath log/mongod.log
 export FLASK_APP=app.py
 export FLASK_DEBUG=1
 flask run
